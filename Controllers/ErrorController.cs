58399c51744871a756128f6741b9fb887c4444e8
John Betancourt <john@intelleges.com>
2015-02-12 20:40:38 +0000

    SEssion timeout

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Generic.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("PageNotFound");
+        }
+
+        public ActionResult PageNotFound()
+        {
+            return View();
+        }
+
+        public ActionResult InternalError()
+        {
+            return View();
+        }
+
+    }
+}
