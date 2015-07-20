63b57e84b529381793ceae3da4f28534f86d8f4c
John Betancourt <john@intelleges.com>
2015-07-20 19:25:13 +0000

    added Models/ContractModel.cs modified Controllers/ContractController.cs modified Views/Contract/Create.cshtml

--- Controllers/ContractController.cs
+++ Controllers/ContractController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Generic.Helpers.Utility;
+using Generic.Helpers;
 
 namespace Generic.Controllers
 {
@@ -19,6 +21,10 @@ namespace Generic.Controllers
         {
             ViewBag.subscriptionStatus = new SelectList(db.subscriptionStatus.ToList(), "id", "description");
             ViewBag.subscriptionType = new SelectList(db.subscriptionType.ToList(), "id", "description");
+            ViewBag.multiTenantProjectType = new SelectList(db.multiTenantProjectType.ToList(), "id", "description");
+            ViewBag.product = new SelectList(db.pr_getProductAll().ToList(), "id", "description");
+            ViewBag.GroupId = new SelectList(db.pr_getGroupByEnterprise(Generic.Helpers.CurrentInstance.EnterpriseID), "id", "name");
+            ViewBag.protocol = new SelectList(db.pr_getProtocolAll(Generic.Helpers.CurrentInstance.EnterpriseID).ToList(), "id", "name");
             return View();
         }
     }
---
+++ Models/ContractModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Generic.Models
+{
+    public class ContractModel
+    {
+        public string customerName { get; set;}
+        public string contractNumber { get; set; }
+        public string primeContractNo { get; set; }
+        public string contractManagerName { get; set; }
+        public string programName { get; set; }
+        public DateTime contractAvardDate { get; set; }
+        public DateTime entryDate { get; set; }
+        public string sequenceNumber { get; set; }
+        public string masterContractNo { get; set; }
+        public int customerAccountAdministrator { get; set; }
+        public int enterprise { get; set; }
+        public int contractType { get; set; }
+        public DateTime contractEndDate { get; set;}
+        public int touchpoint { get; set; }
+        public string SAPOrderNo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string GovContractOfficerPOC { get; set; }
+        public string GCPPOCEmail { get; set; }
+        public string Phone { get; set; }
+    }
+}
