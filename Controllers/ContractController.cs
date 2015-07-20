45d5e841ea7ee86cb04c915e8fddb0916c45dc32
John Betancourt <john@intelleges.com>
2015-07-20 17:19:07 +0000

    added ContractController.cs added Views/Contract/Create.cshtml

---
+++ Controllers/ContractController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Generic.Controllers
+{
+    public class ContractController : Controller
+    {
+        private EntitiesDBContext db = new EntitiesDBContext();
+        // GET: Contract
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.subscriptionStatus = new SelectList(db.subscriptionStatus.ToList(), "id", "description");
+            ViewBag.subscriptionType = new SelectList(db.subscriptionType.ToList(), "id", "description");
+            return View();
+        }
+    }
+}
