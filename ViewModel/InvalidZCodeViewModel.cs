84f8c118b7dc38124aaeece69f2bbe5cd58f07d1
John Betancourt <john@intelleges.com>
2015-11-07 22:30:54 +0000

    View model for Invalid codes

---
+++ ViewModel/InvalidZCodeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Generic.ViewModel
+{
+    public class InvalidZCodeViewModel
+    {
+        public int Id { get; set; }
+        public string Touchpoint { get; set; }
+        public string PartnerType { get; set; }
+        public string ZCode { get; set; }
+        public string ZCodeActionType { get; set; }
+
+    }
+}
