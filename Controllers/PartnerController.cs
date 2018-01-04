9ca8bc3aee98a0f84ae13aa2796264631c958e4c
John Betancourt <john@intelleges.com>
2018-01-04 15:03:16 +0000

    +

--- Controllers/PartnerController.cs
+++ Controllers/PartnerController.cs
@@ -3963,7 +3963,7 @@ namespace Generic.Controllers
 
         [HttpPost]
         [ValidateInput(false)]
-        public ActionResult UpdateIterate(int id, string name, string firstName, string lastName, string phone, string title, string email)
+        public ActionResult UpdateIterate(int id, string name, string firstName, string lastName, string phone, string title, string email, int pptq)
         {
             try
             {
@@ -3973,6 +3973,9 @@ namespace Generic.Controllers
                 db.pr_modifyIteratePartner(ip.id, ip.internalID, name, ip.address1, ip.address2, ip.city, ip.state, ip.zipcode, ip.country, ip.dunsnumber, ip.federalID, ip.numberOfEmployees, ip.annualRevenue, ip.status, ip.owner, ip.author, ip.dateApproved, ip.active, ip.dateAdded, null, ip.lastModified, ip.person, ip.note);
 
                 db.pr_modifyIteratePerson(ipr.id, firstName, lastName, title, email, phone, ipr.fax, ipr.active, ipr.dateAdded, ipr.lastModified, ipr.iteratePartner, ipr.lastContact, ipr.lastContactDate, ipr.previousContact, ipr.previousContactDate, ipr.nextAction, ipr.nextActionDate, ipr.notes, null);
+
+                var partner = db.pr_getPartnerByPPTQ(pptq).First();
+                db.pr_modifyPartner(partner.id, Generic.Helpers.CurrentInstance.EnterpriseID, partner.internalID, name, partner.address1, partner.address2, partner.city, partner.state, partner.province, partner.zipcode, partner.country, phone, partner.fax, firstName, lastName, title, email, partner.dunsNumber, partner.federalID, partner.status, partner.loadHistory, partner.owner, partner.author, partner.dateApproved, partner.active, partner.lastModified);
             }
             catch (Exception ex)
             {
