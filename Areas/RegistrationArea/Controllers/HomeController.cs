4868e59db39aa020c215ec9965fe82fe0f33d823
John Betancourt <john@intelleges.com>
2018-06-15 15:06:47 +0000

    +

--- Areas/RegistrationArea/Controllers/HomeController.cs
+++ Areas/RegistrationArea/Controllers/HomeController.cs
@@ -1093,6 +1093,9 @@ namespace Generic.Areas.RegistrationArea.Controllers
 
             if (responseId != -1)
             {
+                if (db.pr_getApprovalEmailStatus(pptqObj.id, questionId, 74).First() > 0)
+                    return;
+
                 var p = db.pr_getPersonByEmail(1137, emailTo).FirstOrDefault();
                 if (p == null)
                 {
