049c610346f429a8a1047707e4097dff5a7980db
John Betancourt <john@intelleges.com>
2019-01-21 05:15:35 +0000

    change for signature alerity

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace Generic
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                     "~/Scripts/jquery-1.9.1.js", "~/Scripts/jquery-migrate-1.2.1.js", "~/Scripts/alertify.js", "~/Scripts/jquery.blockUI.js"));
+                     "~/Scripts/jquery-1.9.1.js", /*"~/Scripts/jquery-migrate-1.2.1.js",*/ "~/Scripts/alertify.js", "~/Scripts/jquery.blockUI.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
 						"~/Scripts/jquery-ui-1.9.2.js", "~/Scripts/jquery.ui-contextmenu.js"));
