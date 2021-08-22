04ad10f342dc6bb1b920e95db839dfb4d12ffead
John Betancourt <john@intelleges.com>
2021-08-22 20:02:55 +0000

    fix: added missing datetime

--- Controllers/PartnerController.cs
+++ Controllers/PartnerController.cs
@@ -2875,7 +2875,7 @@ namespace Generic.Controllers
             var questionnaire = db.pr_getQuestionnaireByPPTQ(pptq.id).FirstOrDefault();
             var appQuestion = db.pr_getApprovalRequestQuestionByQuestionnaire(questionnaire.id).FirstOrDefault();
             db.pr_modifyPartnerPartnertypeTouchpointQuestionnaireStatus(pptq.id, (int)PartnerStatus.Responded_Complete);
-            db.pr_addPartnerPartnertypeTouchpointQuestionnairePersonApproval(pptq.id, SessionSingleton.LoggedInUserId, (int)PartnerStatus.Approved_By_Manager, null, null);
+            db.pr_addPartnerPartnertypeTouchpointQuestionnairePersonApproval(pptq.id, SessionSingleton.LoggedInUserId, (int)PartnerStatus.Approved_By_Manager, null, DateTime.Now);
             var list = db.pr_validateEmailAlertListQuestionResponseByPPTQ(pptq.id, appQuestion.emailAlertList.Split(new string[] { "where:" }, StringSplitOptions.RemoveEmptyEntries)[1].Replace(";", "").Trim() + ";").ToList();
             string qsss = "";
             foreach (var item in list) qsss += db.pr_getQuestionTitle(item.Question).First() + "<br/>";
